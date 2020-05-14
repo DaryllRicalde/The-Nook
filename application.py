1d935cb7be8ab5e4040beb944f58dd6868f1eadf
Daryll Ricalde <rstevendaryll@gmail.com>
2020-05-14 22:37:10 +1000

    added models.py with Users and Books class

--- application.py
+++ application.py
@@ -5,8 +5,12 @@ from flask import Flask, session, render_template, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db.init_app(app)
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
--- models.py
+++ models.py
@@ -4,13 +4,14 @@ db = SQLAlchemy()
 
 class Users(db.Model):
     __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    origin = db.Column(db.String, nullable=False)
-    destination = db.Column(db.String, nullable=False)
-    duration = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
 
 
 class Books(db.Model):
     __tablename__ = "books"
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    isbn = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    author = db.Column(db.String, nullable=False)
+    year = db.Column(db.String, nullable=False)
