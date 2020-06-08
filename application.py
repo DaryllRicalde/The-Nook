29439527a38d50502c206beda98968afd5cd9552
DaryllRicalde <rstevendaryll@gmail.com>
2020-06-08 19:00:22 +1000

    users are now able to login and be redirected to a personalised menu page

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import os
 import requests
 
-from flask import Flask, session, render_template, request,json,redirect
+from flask import Flask, session, render_template, request,json,redirect,url_for
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -11,7 +11,7 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
-Session(app)
+
 
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
@@ -20,6 +20,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 # Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
@@ -41,7 +42,9 @@ def index():
             return render_template("error.html",message="Please enter a valid username")
         
         #Check if the password or verify password field has input
-        if password or verify == "":
+        if password == "":
+            return render_template("error.html",message="Please enter a valid password")
+        if verify == "":
             return render_template("error.html",message="Please enter a valid password")
 
         #Check if username already exists
@@ -50,11 +53,11 @@ def index():
             return render_template("error.html",message="That username is already taken")
         
         #If username is valid, check if passwords are matching and if TRUE, insert the user into the database
-        elif password == verify:
+        if password == verify:
             db.execute("INSERT INTO users (username,password) VALUES (:username, :password)", {"username":name,"password":password})
             db.commit()
             return redirect("/login")
-        elif password != verify:
+        else:
             return render_template("error.html", message="Passwords dont match")
     if request.method == "GET":
         return render_template("index.html")
@@ -65,24 +68,35 @@ def login():
     #Check if user is submitting data via POST
     if request.method == "POST":
 
-        #Check if the user exists
+        #Check if this user exists
         name = request.form.get("user")
         password = request.form.get("password")
 
-        user = db.execute("SELECT * FROM users WHERE username = :username AND (password =:password)", {"username":name, "password":password})
-        if user.rowCount > 0:
-            session["user"] = checkU
-            return redirect("/menu")
+        user = db.execute("SELECT * FROM users WHERE username = :username AND (password =:password)", {"username":name, "password":password}).fetchone()
 
-    return render_template("login.html")
+        #If the user does not exist or the password and username combination are wrong
+        if user is None:
+            return render_template("error.html", message="User does not exist, ensure that both username and password are correct.")
 
-@app.route("/menu", methods=["GET","POST"]) 
-def menu():
-    
-    #Check if a valid user is trying send a GET request
+        else:
+            user_id = user.user_id 
+            return redirect(url_for("menu", user_id=user_id))
+
+    #Check if user is wanting to access the page via GET           
     if request.method == "GET":
+        return render_template("login.html")
 
-        return render_template("menu.html")
+@app.route("/menu/<user_id>", methods=["GET","POST"]) 
+def menu(user_id):
+    
+    #Check if a valid user is trying to access the menu page via GET
+    if request.method == "GET":
+        if user_id is None:
+            return render_template("error.html",message="Please login first")
+        else:
+            currUser = db.execute("SELECT * FROM users WHERE user_id =:user_id", {"user_id":user_id}).fetchone()
+            name = currUser.username
+            return render_template("menu.html",Username=name)
 
 @app.route("/results", methods=["POST"])
 def results():
